ce30468f2dfd0fc390cea125538693867c38b7d6
sidneyfjr <sidneyfrancajr@gmail.com>
2023-07-16 16:42:22 -0300

    Primeiro commit

---
+++ goclient/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"encoding/json"
+	"github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
+)
+
+var (
+	clientID     = "myclient"
+	clientSecret = "60831853-3987-4a98-b069-0e932dbcdf2a"
+)
+
+func main() {
+	ctx := context.Background()
+
+	provider, err := oidc.NewProvider(ctx, "http://host.docker.internal:8080/auth/realms/myrealm")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  "http://host.docker.internal:8081/auth/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "roles"},
+	}
+
+	state := "123"
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		http.Redirect(writer, request, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	http.HandleFunc("/auth/callback", func(writer http.ResponseWriter, request *http.Request){
+		if request.URL.Query().Get("state") != state {
+			http.Error(writer, "State inválido", http.StatusBadRequest)
+			return
+		}
+
+		token, err := config.Exchange(ctx, request.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(writer, "Falha ao trocar o token", http.StatusInternalServerError)
+			return
+		}
+
+		idToken, ok := token.Extra("id_token").(string)
+		if !ok {
+			http.Error(writer, "Falha ao gerar o IDToken", http.StatusInternalServerError)
+			return
+		}
+
+		userInfo, err := provider.UserInfo(ctx, oauth2.StaticTokenSource(token))
+		if err != nil {
+			http.Error(writer, "Falha ao pegar UserInfo", http.StatusInternalServerError)
+			return
+		}
+
+		resp := struct {
+			AccessToken *oauth2.Token 
+			IDToken string
+			UserInfo *oidc.UserInfo
+		}{
+			AccessToken: token,
+			IDToken: idToken,
+			UserInfo: userInfo,
+		}
+
+		data, err := json.Marshal(resp)
+
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		writer.Write(data)
+
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
